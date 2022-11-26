18370867346a523bd01a8ee3a131248bf4d9d2bc
RedEye75 <yusufbinemamstk@gmail.com>
2022-11-27 03:47:32 +0600

    get bookings data from db

--- index.js
+++ index.js
@@ -51,6 +51,13 @@ async function run() {
       res.send(options);
     });
 
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingCollections.find(query).toArray();
+      res.send(bookings);
+    });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       console.log(booking);
